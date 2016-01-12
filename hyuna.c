5aa74adbff9cca20708f91fb2e2dea8582db4bd0
HyunA <khyun9764@naver.com>
2016-01-12 16:35:44 +0900

    create "Hyuna" program

--- hyuna.c
+++ hyuna.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
+//For command line git tutorial
+//GitHub modification
 int main()
 {
 printf("This is hyuna's computer");
+printf("Today is second day of using github");
+printf("One more time");
+printf("DDDD");
 }
 
